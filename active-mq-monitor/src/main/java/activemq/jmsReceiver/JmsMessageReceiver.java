1d6f2bcc53829380bf70c4652f6b76acaec26dff
Nithin Bharadwaja Mandipati <mandipat@deshaw.com>
2016-10-08 06:59:20 -0400

    Adding Listener configuration

--- active-mq-monitor/src/main/java/activemq/jmsReceiver/JmsMessageReceiver.java
+++ active-mq-monitor/src/main/java/activemq/jmsReceiver/JmsMessageReceiver.java
@@ -1,78 +1,23 @@
-/*
- * JmsMessageReceiver.java
- * 
- */
-
-/*
- * Copyright (c) 2016 D. E. Shaw & Co., L.P. All rights reserved.
- *  
- * This software is the confidential and proprietary information
- * of D. E. Shaw & Co., L.P. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with D. E. Shaw & Co., L.P.
- */
-
 package activemq.jmsReceiver;
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.Destination;
-import javax.jms.JMSException;
 import javax.jms.Message;
-import javax.jms.MessageConsumer;
 import javax.jms.MessageListener;
-import javax.jms.Session;
+import javax.jms.ObjectMessage;
 import javax.jms.TextMessage;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-
-/**
- * @author kabraa
- */
 public class JmsMessageReceiver implements MessageListener {
 
-    public static String brokerURL = "tcp://localhost:61618";
+	@Override
+	public void onMessage(Message message) {
+		if(message  instanceof TextMessage ) {
+			System.out.println(message.toString());
+		} else if(message instanceof ObjectMessage) {
+			System.out.println(message.toString());
+			
+		} else {
+			System.out.println("what the fuck are you sending !!");
+		}
 
-    private ConnectionFactory factory;
+	}
 
-    private Connection connection;
-
-    private Session session;
-
-    private MessageConsumer consumer;
-
-    public static void main(String[] args) {
-        JmsMessageReceiver app = new JmsMessageReceiver();
-        app.run();
-    }
-
-    public void run() {
-        try {
-            ConnectionFactory factory = new ActiveMQConnectionFactory(brokerURL);
-            connection = factory.createConnection();
-            connection.start();
-            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Destination destination = session.createQueue("test");
-            consumer = session.createConsumer(destination);
-            consumer.setMessageListener(this);
-        } catch (Exception e) {
-            System.out.println("Caught:" + e);
-            e.printStackTrace();
-        }
-    }
-
-    public void onMessage(Message message) {
-        try {
-            if (message instanceof TextMessage) {
-                TextMessage txtMessage = (TextMessage) message;
-                System.out.println("Message received: " + txtMessage.getText());
-            } else {
-                System.out.println("Invalid message received.");
-            }
-        } catch (JMSException e) {
-            System.out.println("Caught:" + e);
-            e.printStackTrace();
-        }
-    }
 }
