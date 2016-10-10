00b84c55cae874712b52161f9b289a0bd5ada774
Nithin Bharadwaja Mandipati <mandipat@deshaw.com>
2016-10-10 02:03:57 -0400

    removing banner comments

--- active-mq-monitor/src/main/java/activemq/jms/domain/MessageListner.java
+++ active-mq-monitor/src/main/java/activemq/jms/domain/MessageListner.java
@@ -3,15 +3,6 @@
  * 
  */
 
-/*
- * Copyright (c) 2016 D. E. Shaw & Co., L.P. All rights reserved.
- *  
- * This software is the confidential and proprietary information
- * of D. E. Shaw & Co., L.P. ("Confidential Information").  You
- * shall not disclose such Confidential Information and shall use
- * it only in accordance with the terms of the license agreement
- * you entered into with D. E. Shaw & Co., L.P.
- */
 
 package activemq.jms.domain;
 
